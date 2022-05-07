8dc664019386f9fae91cd683a556d303e76c8fe6
Alex <obodnarchuk@outlook.com>
2022-05-07 18:12:22 +0200

    finished prep project, started BASIC AUTH

---
+++ spring-security-demo-02-basic-security/src/main/java/pl/alex/springsecurity/config/DemoAppConfig.java
@@ -0,0 +1,23 @@
+package pl.alex.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pl.alex.springsecurity")
+public class DemoAppConfig {
+
+	// view resolver will show Spring where to look for the files
+	@Bean
+	public ViewResolver createViewResolver(){
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		return  viewResolver;
+	}
+}
